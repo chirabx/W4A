d95c1c14453b607998d3bc5f47396c8065e3d3c8
chirabx <chirabx@gmail.com>
2025-09-27 14:40:21 +0800

    init

--- shoot_robot_code_202510/src/shoot_robot/src/shoot_robot.cpp
+++ shoot_robot_code_202510/src/shoot_robot/src/shoot_robot.cpp
@@ -4,152 +4,81 @@
 #include <iostream>
 #include <geometry_msgs/Quaternion.h>
 #include <tf2/LinearMath/Quaternion.h>
+#include <vector>
 
 using namespace std;
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
+// 射击点结构体
+struct ShootPoint
+{
+    double x, y, yaw;
+    std::string description;
+    std::string launch_file;
+};
+
+// 导航到指定点并执行射击的辅助函数
+bool navigateAndShoot(MoveBaseClient &ac, const ShootPoint &point, int goal_number)
+{
+    // 设置目标点
+    move_base_msgs::MoveBaseGoal goal;
+    tf2::Quaternion quaternion;
+    quaternion.setRPY(0, 0, point.yaw);
+
+    goal.target_pose.pose.position.x = point.x;
+    goal.target_pose.pose.position.y = point.y;
+    goal.target_pose.pose.orientation.z = quaternion.z();
+    goal.target_pose.pose.orientation.w = quaternion.w();
+    goal.target_pose.header.frame_id = "map";
+    goal.target_pose.header.stamp = ros::Time::now();
+
+    // 发送目标并等待结果
+    ac.sendGoal(goal);
+    ROS_INFO("Send Goal %d: %s", goal_number, point.description.c_str());
+    ac.waitForResult();
+
+    if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    {
+        ROS_INFO("Goal %d reached successfully!", goal_number);
+        if (!point.launch_file.empty())
+        {
+            std::string command = "roslaunch shoot_robot " + point.launch_file;
+            system(command.c_str());
+        }
+        return true;
+    }
+    else
+    {
+        ROS_WARN("Goal %d planning failed for some reason", goal_number);
+        return false;
+    }
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "send_goals_node");
-
     MoveBaseClient ac("move_base", true);
-
     ac.waitForServer();
 
-    // 选择离自己最近的 4 个普通靶标位置
-    move_base_msgs::MoveBaseGoal goal1;
+    // 定义所有射击点
+    std::vector<ShootPoint> shoot_points = {
+        // 普通靶标射击点
+        {0.5, 0.0, -1.5707, "第1个普通靶标(右转90度)", "shoot_tag_1.launch"},
+        {1.0, 0.0, -1.5707, "第2个普通靶标(右转90度)", "shoot_tag_1.launch"},
+        {1.0, 0.0, 0.0, "第3个普通靶标(正前方)", "shoot_tag_1.launch"},
+        {0.5, 0.0, 1.5707, "第4个普通靶标(左转90度)", "shoot_tag_1.launch"},
+        // 敌方基地射击点
+        {1.0, -2.0, -1.5707, "敌方基地(右转90度)", "shoot_tag_2.launch"},
+        // 返回起始点
+        {0.0, 0.0, 0.0, "返回起始点", ""}};
 
-    // 对方基地点位
-    move_base_msgs::MoveBaseGoal goal2;
-
-    // 返回点位
-    move_base_msgs::MoveBaseGoal goal3;
-
-    // 第一个普通靶标在 map 坐标系下的坐标位置，前方0.5米，向右90度
-    tf2::Quaternion quaternion;
-    quaternion.setRPY(0, 0, -1.5707);
-    goal1.target_pose.pose.position.x = 0.5;
-    goal1.target_pose.pose.position.y = 0.0;
-    goal1.target_pose.pose.orientation.z = quaternion.z();
-    goal1.target_pose.pose.orientation.w = quaternion.w();
-    goal1.target_pose.header.frame_id = "map";
-    goal1.target_pose.header.stamp = ros::Time::now();
-    ac.sendGoal(goal1);
-    ROS_INFO("Send Goal  1 !!!");
-    ac.waitForResult();
-    if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    // 依次执行所有射击点
+    for (size_t i = 0; i < shoot_points.size(); ++i)
     {
-        ROS_INFO("The Goal 1 Reached Successfully!!!");
-        system("roslaunch shoot_robot shoot_tag_1.launch");
-    }
-    else
-    {
-        ROS_WARN("The Goal Planning Failed for some reason");
+        navigateAndShoot(ac, shoot_points[i], i + 1);
     }
 
-    // 第二个普通靶标在 map 坐标系下的坐标位置，前方1.0米，向右90度
-    goal1.target_pose.pose.position.x = 1.0;
-    goal1.target_pose.pose.position.y = 0.0;
-    goal1.target_pose.pose.orientation.z = quaternion.z();
-    goal1.target_pose.pose.orientation.w = quaternion.w();
-    goal1.target_pose.header.frame_id = "map";
-    goal1.target_pose.header.stamp = ros::Time::now();
-    ac.sendGoal(goal1);
-    ROS_INFO("Send Goal 2 !!!");
-    ac.waitForResult();
-    if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    {
-        ROS_INFO("The Goal 2 Reached Successfully!!!");
-        system("roslaunch shoot_robot shoot_tag_1.launch");
-    }
-    else
-    {
-        ROS_WARN("The Goal Planning Failed for some reason");
-    }
-
-    // 第三个普通靶标在 map 坐标系下的坐标位置，前方1.0米，面向正前方
-    quaternion.setRPY(0, 0, 0);
-    goal1.target_pose.pose.position.x = 1.0;
-    goal1.target_pose.pose.position.y = 0.0;
-    goal1.target_pose.pose.orientation.z = quaternion.z();
-    goal1.target_pose.pose.orientation.w = quaternion.w();
-    goal1.target_pose.header.frame_id = "map";
-    goal1.target_pose.header.stamp = ros::Time::now();
-    ac.sendGoal(goal1);
-    ROS_INFO("Send Goal 3 !!!");
-    ac.waitForResult();
-    if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    {
-        ROS_INFO("The Goal 3 Reached Successfully!!!");
-        system("roslaunch shoot_robot shoot_tag_1.launch");
-    }
-    else
-    {
-        ROS_WARN("The Goal Planning Failed for some reason");
-    }
-
-
-    // 第四个普通靶标在 map 坐标系下的坐标位置，前方0.5米，面向左侧
-    quaternion.setRPY(0, 0, 1.5707);
-    goal1.target_pose.pose.position.x = 0.5;
-    goal1.target_pose.pose.position.y = 0.0;
-    goal1.target_pose.pose.orientation.z = quaternion.z();
-    goal1.target_pose.pose.orientation.w = quaternion.w();
-    goal1.target_pose.header.frame_id = "map";
-    goal1.target_pose.header.stamp = ros::Time::now();
-    ac.sendGoal(goal1);
-    ROS_INFO("Send Goal 4 !!!");
-    ac.waitForResult();
-    if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    {
-        ROS_INFO("The Goal 4 Reached Successfully!!!");
-        system("roslaunch shoot_robot shoot_tag_1.launch");
-    }
-    else
-    {
-        ROS_WARN("The Goal Planning Failed for some reason");
-    }
-
-    // 对方基地在 map 坐标系下的坐标位置,前方 1 米，右侧2米，朝向右侧
-    quaternion.setRPY(0, 0, -1.5707);
-    goal2.target_pose.pose.position.x = 1.0;
-    goal2.target_pose.pose.position.y = -2.0;
-    goal2.target_pose.pose.orientation.z = quaternion.z();
-    goal2.target_pose.pose.orientation.w = quaternion.w();
-    goal2.target_pose.header.frame_id = "map";
-    goal2.target_pose.header.stamp = ros::Time::now();
-    ac.sendGoal(goal2);
-    ROS_INFO("Send Goal Enemy Target !!!");
-    ac.waitForResult();
-    if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    {
-        ROS_INFO("The Goal Enemy Target Reached Successfully!!!");
-        system("roslaunch shoot_robot shoot_tag_2.launch");
-    }
-    else
-    {
-        ROS_WARN("The Goal Planning Failed for some reason");
-    }
-
-    // 返回出发点
-    goal3.target_pose.pose.position.x = 0.0;
-    goal3.target_pose.pose.position.y = 0.0;
-    goal3.target_pose.pose.orientation.z = 0.0;
-    goal3.target_pose.pose.orientation.w = 1.0;
-    goal3.target_pose.header.frame_id = "map";
-    goal3.target_pose.header.stamp = ros::Time::now();
-    ac.sendGoal(goal3);
-    ROS_INFO("Send Goal Home !!!");
-    ac.waitForResult();
-    if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    {
-        ROS_INFO("Back !!!!");
-    }
-    else
-    {
-        ROS_WARN("The Goal Planning Failed for some reason");
-    }
-
+    ROS_INFO("All shooting tasks completed!");
     return 0;
 }

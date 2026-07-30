843da13bd837e7e326f9a117b13a82b05a171fa5
chirabx <1819496201@qq.com>
2026-07-30 18:00:53 +0800

    改了导航点，加后退，改了出发方式

--- shoot_robot_code_202510/src/shoot_robot/src/shoot_robot.cpp
+++ shoot_robot_code_202510/src/shoot_robot/src/shoot_robot.cpp
@@ -39,6 +39,10 @@ void Move_safe(ros::Publisher &pub, double linear_x, double linear_y, double dis
     vel_msg.linear.y = 0.0;
     pub.publish(vel_msg);
 }
+<<<<<<< HEAD
+=======
+
+>>>>>>> 改了导航点，加后退，改了出发方式
 
 // Retry logic function
 void performRetryLogic(MoveBaseClient &ac, double x, double y, double yaw, const std::string &tag_name)
@@ -134,8 +138,11 @@ int main(int argc, char **argv)
     int count = 0;
     ros::Rate loop_rate(10);
     shoot_close_client.call(empty_srv);
+    Move_safe(pub, 0.0, 0.30, 25);
+    Move_safe(pub, 0.30, 0.0, 27);
 
     // First target point
+<<<<<<< HEAD
     
     
     
@@ -178,6 +185,20 @@ int main(int argc, char **argv)
     // //Third target point
    
     
+=======
+    Move2goal(ac, 2.308, 0.628, 0.785, "1");  //G
+    shoot_close_client.call(empty_srv);
+
+
+    // //Second target point
+    Move2goal(ac, 2.284, -0.09, -0.785, "1");  //H
+    shoot_close_client.call(empty_srv);
+
+    
+   
+
+    // //Third target point0
+>>>>>>> 改了导航点，加后退，改了出发方式
     Move2goal(ac, 1.538, -0.009, -2.355, "1");  // I
     shoot_close_client.call(empty_srv);
 
@@ -196,6 +217,7 @@ int main(int argc, char **argv)
     //Move2goal(ac, 1.633, 2.316, 2.322, "1");  //D
     //shoot_close_client.call(empty_srv);
 
+<<<<<<< HEAD
     
     //Move1goal(ac, 1.100, 0.400, 0);
 
@@ -212,11 +234,26 @@ int main(int argc, char **argv)
     
     // Seventh target point
     Move2goal(ac, 2.279, 1.543, -0.785, "1");//F
+=======
+
+    // Fifth target point
+    Move2goal(ac, 2.306, 2.106, 0.785, "1");  //E
+    shoot_close_client.call(empty_srv);
+
+    
+
+    // // Sixth target point
+    Move2goal(ac, 2.279, 1.543, -0.785, "1");//F
+    shoot_close_client.call(empty_srv);
+    // Seventh target point
+    Move2goal(ac, 0.083, 1.707, 3.92, "1");  //A
+>>>>>>> 改了导航点，加后退，改了出发方式
     shoot_close_client.call(empty_srv);
     //Move2goal(ac, 1.633, 2.316, 2.322, "1");  //D
     //shoot_close_client.call(empty_srv);
 
     
+<<<<<<< HEAD
     Move2goal(ac, 0.053, 1.647, 2.355, "1");  //A
     shoot_close_client.call(empty_srv);
    // Move1goal(ac, 0.877, 0.3, 1.57);
@@ -252,6 +289,22 @@ int main(int argc, char **argv)
     //shoot_close_client.call(empty_srv);
     Move2goal(ac, 0.855, 2.258, 0.83, "1");  //C Move2goal(ac, 0.899, 2.308, 0.83, "1");  
     shoot_close_client.call(empty_srv);
+=======
+
+    // Eighth target point
+    Move2goal(ac, 0.075, 2.364, 2.355, "1");  //B
+    shoot_close_client.call(empty_srv);
+    Move_safe(pub, -0.2, 0.0, 8);
+    //Move1goal(ac, 0.5, 2.2, 0); 
+    // Enemy base
+    // Move2goal(ac, 2.412, 1.544, 0.785, "3");
+    Move2goal(ac, 0.66, 2.1, 0.83, "1");  //C 
+    shoot_close_client.call(empty_srv);
+
+    Move1goal(ac, 0.03, 0.03, 0.0);
+    Move_safe(pub, 0.0, -0.2, 15);
+    //Move_safe(pub, -0.2, 0.0, 12);
+>>>>>>> 改了导航点，加后退，改了出发方式
 
     // vel_msg.linear.x = -0.05;
     // count = 0;
